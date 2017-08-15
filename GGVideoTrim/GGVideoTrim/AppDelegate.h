74c865c4c779bfcae653a29a679399bbccc5bf8c
DoctorGG <bjzhaoyingru@corp.netease.com>
2017-08-15 14:39:25 +0800

    Release 1.0.0.

---
+++ GGVideoTrim/GGVideoTrim/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GGVideoTrim
+//
+//  Created by DoctorGG on 2017/8/15.
+//  Copyright © 2017年 NETEASE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
